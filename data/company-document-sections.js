28c35b5fe41706a0c3061f4ffae9dbde73058ba6
Stephen Yuan <stephen@merlingroup.co>
2016-11-14 13:31:07 +1100

    Update company-document-sections to use config.

--- data/company-document-sections.js
+++ data/company-document-sections.js
@@ -1,62 +1,64 @@
+import ENV_VARS from '../env-vars'
+
 const COMPANY_DOCUMENT_SECTIONS = [
     {
         name: 'Partner Enquiry Form',
         description: 'Signed document to commerce partnership with Merlin Group',
-        tag: 'company.document.PartnerEnquiryForm',
+        tag: ENV_VARS.DOCUMENT_TAG.company_document + '.PartnerEnquiryForm',
         required: true
     },
     {
         name: 'Accountant\'s Letter',
         description: 'Supporting this application',
-        tag: 'company.document.AccountantLetter',
+        tag: ENV_VARS.DOCUMENT_TAG.company_document + '.AccountantLetter',
         required: true
     },
     {
         name: 'Last 2 Years Financials',
         description: 'Including P&L and Balance Sheet',
-        tag: 'company.document.Last2YearsFinancials',
+        tag: ENV_VARS.DOCUMENT_TAG.company_document + '.Last2YearsFinancials',
         required: true
     },
     {
         name: 'BAS Statement',
         description: 'Last 4 quarter',
-        tag: 'company.document.BASStatement',
+        tag: ENV_VARS.DOCUMENT_TAG.company_document + '.BASStatement',
         required: true
     },
     {
         name: 'Lease Agreement',
         description: '',
-        tag: 'company.document.LeaseAgreement',
+        tag: ENV_VARS.DOCUMENT_TAG.company_document + '.LeaseAgreement',
         required: true
     },
     {
         name: 'Any Immigration Paperwork for Previous Applications',
         description: '',
-        tag: 'company.document.ImmigrationPaperworkForPreviousApplications',
+        tag: ENV_VARS.DOCUMENT_TAG.company_document + '.ImmigrationPaperworkForPreviousApplications',
         required: false
     },
     {
         name: 'Evidence of Customers',
         description: 'Customer invoices, jobs in-progress, tender documents, project drawings, growth plans/financial forecasts',
-        tag: 'company.document.EvidenceOfCustomers',
+        tag: ENV_VARS.DOCUMENT_TAG.company_document + '.EvidenceOfCustomers',
         required: true
     },
     {
         name: 'Company Trust Deed',
         description: 'If you have a TRUST',
-        tag: 'company.document.CompanyTrustDeed',
+        tag: ENV_VARS.DOCUMENT_TAG.company_document + '.CompanyTrustDeed',
         required: false
     },
     {
         name: 'Examples of Employment Contracts and Staff Payroll Data',
         description: '',
-        tag: 'company.document.ExamplesOfEmploymentContractsAndStaffPayrollData',
+        tag: ENV_VARS.DOCUMENT_TAG.company_document + '.ExamplesOfEmploymentContractsAndStaffPayrollData',
         required: true
     },
     {
         name: 'Any Previous Labour Agreements',
         description: '',
-        tag: 'company.document.PreviousLabourAgreements',
+        tag: ENV_VARS.DOCUMENT_TAG.company_document + '.PreviousLabourAgreements',
         required: false
     }
 ]
