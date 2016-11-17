837f9669308b1c6cbc165dfb59fdd7c6d106fec4
Roger Liang <roger@merlingroup.co>
2016-11-17 03:25:18 +0000

    fix address form ref for ui helper

--- react/components/address-form.js
+++ react/components/address-form.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 
-import Util from '../utils'
+import uiHelper from '../../utils/ui-helper'
 import { Input } from '../elements/input'
 import { StateSelect, CountrySelect } from '../elements/select'
 
@@ -18,8 +18,8 @@ class AddressForm extends Component {
                     <label className="required">Address Line 1</label>
                     <Input className=""
                             type="text"
-                            name={Util.makeName(this.props.namePrefix, "address_line_1")}
-                            value={Util.valOr(this.props.address.address_line_1)}
+                            name={uiHelper.makeName(this.props.namePrefix, "address_line_1")}
+                            value={uiHelper.valOr(this.props.address.address_line_1)}
                             onChange={this.props.onUserInput}
                             required={true}
                             />
@@ -28,8 +28,8 @@ class AddressForm extends Component {
                     <label className="required">Address Line 2</label>
                     <Input className=""
                             type="text"
-                            name={Util.makeName(this.props.namePrefix, "address_line_2")}
-                            value={Util.valOr(this.props.address.address_line_2)}
+                            name={uiHelper.makeName(this.props.namePrefix, "address_line_2")}
+                            value={uiHelper.valOr(this.props.address.address_line_2)}
                             onChange={this.props.onUserInput}
                             />
                 </li>
@@ -37,8 +37,8 @@ class AddressForm extends Component {
                     <label className="required">Suburb</label>
                     <Input className=""
                             type="text"
-                            name={Util.makeName(this.props.namePrefix, "suburb")}
-                            value={Util.valOr(this.props.address.suburb)}
+                            name={uiHelper.makeName(this.props.namePrefix, "suburb")}
+                            value={uiHelper.valOr(this.props.address.suburb)}
                             onChange={this.props.onUserInput}
                             required={true}
                             />
@@ -46,8 +46,8 @@ class AddressForm extends Component {
                 <li className="l_span_3">
                     <label className="required">State</label>
                     <StateSelect
-                            name={Util.makeName(this.props.namePrefix, "state")}
-                            value={Util.valOr(this.props.address.state)}
+                            name={uiHelper.makeName(this.props.namePrefix, "state")}
+                            value={uiHelper.valOr(this.props.address.state)}
                             onChange={this.props.onUserInput}
                             required={true}
                             />
@@ -56,8 +56,8 @@ class AddressForm extends Component {
                     <label className="required">Post Code</label>
                     <Input className=""
                             type="number"
-                            name={Util.makeName(this.props.namePrefix, "postcode")}
-                            value={Util.valOr(this.props.address.postcode)}
+                            name={uiHelper.makeName(this.props.namePrefix, "postcode")}
+                            value={uiHelper.valOr(this.props.address.postcode)}
                             onChange={this.props.onUserInput}
                             required={true}
                             maxLength={5}
@@ -66,8 +66,8 @@ class AddressForm extends Component {
                 <li className="l_half">
                     <label className="required">Country</label>
                     <CountrySelect
-                            name={Util.makeName(this.props.namePrefix, "country")}
-                            value={Util.valOr(this.props.address.country)}
+                            name={uiHelper.makeName(this.props.namePrefix, "country")}
+                            value={uiHelper.valOr(this.props.address.country)}
                             onChange={this.props.onUserInput}
                             required={true}
                             />
