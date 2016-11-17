b60298df2f295dd2b37f0accd689f606f6d8a99a
nickmerlin <nickheskes@Nicks-MacBook-Pro.local>
2016-11-17 15:13:16 +1100

    merge master

--- react/components/default-register-form.js
+++ react/components/default-register-form.js
@@ -12,47 +12,47 @@ const DefaultRegisterForm = (RegisterComponent, context) => (
     {
         //callback is the RegisterComponent scope this
         (callback) => (
-            <form className="" onSubmit={callback.register}>
+            <form id="register-form" className="bordered_box" onSubmit={callback.register}>
                 <fieldset>
-                    <ul className="">
-                        <li className="">
-                            <label className="" id="">First name</label>
-                            <input type="text" className="" onChange={callback.setStateVal} name="given_name" id="given_name" required="" />
-                            {/*<div className="">Please enter a first name</div>*/}
-                        </li>
-                        <li className="">
-                            <label className="" id="">Last name</label>
-                            <input type="text" className="" onChange={callback.setStateVal} name="family_name" id="family_name" required="" />
-                            {/*<div className="">Please enter a last name</div>*/}
-                        </li>
-                        <li className="">
-                            <label className="" id="">Email address</label>
-                            <input type="email" className="" onChange={callback.setStateVal} name="email" id="email" required="" />
-                            {/*<div className="">Please enter an email address</div>*/}
-                        </li>
-                        <li className="">
-                            <label className="" id="">Phone number</label>
-                            <input type="tel" className="" onChange={callback.setStateVal} name="phone_number" id="phone_number" required="" />
-                            {/*<div className="">Please enter an email address</div>*/}
-                        </li>
-                        <li className="">
-                            <label className="required">Password</label>
-                            <div className="password-container">
-                                <Password onChange={callback.setStateVal} password={callback.getStateVal('password')}/>
-                            </div>
-                        </li>
-                        <li>
-                            By creating an account you agree to the <a href="#" target="_blank">Terms &amp; Conditions</a> and <a href="#" target="_blank">Privacy Policy</a>.
-                        </li>
-                        <li className="">
-                            <div className="">
-                              <button className="" type="submit" id="create">Create an Agent Account</button>
-                            </div>
-                        </li>
-                    </ul>
+                <ul className="form-list">
+                    <li>
+                        <label className="required">First name</label>
+                        <input type="text" className="input-text" onChange={callback.setStateVal} name="given_name" id="given_name" required="" />
+                        <div className="error">Please enter a first name</div>
+                    </li>
+                    <li className="">
+                        <label className="required">Last name</label>
+                        <input type="text" className="input-text" onChange={callback.setStateVal} name="family_name" id="family_name" required="" />
+                        <div className="error">Please enter a last name</div>
+                    </li>
+                    <li className="">
+                        <label className="required">Email address</label>
+                        <input type="email" className="input-text" onChange={callback.setStateVal} name="email" id="email" required="" />
+                        <div className="error">Please enter an email address</div>
+                    </li>
+                    <li className="">
+                        <label className="required">Phone number</label>
+                        <input type="tel" className="input-text" onChange={callback.setStateVal} name="phone_number" id="phone_number" required="" />
+                        <div className="error">Please enter an email address</div>
+                    </li>
+                    <li className="">
+                        <label className="required">Password</label>
+                        <input type="password" className="input-text" onChange={callback.setStateVal} name="password" id="password" required="" />
+                        <div className="password-show">Show</div>
+                        <div className="error">Please enter a password</div>
+                    </li>
+                    <li>
+                    By creating an account you agree to the <a href="#" target="_blank">Terms &amp; Conditions</a> and <a href="#" target="_blank">Privacy Policy</a>.
+                    </li>
+                    <li className="">
+                        <div className="">
+                          <button className="btn btn_primary btn_full" type="submit" id="create">Create an Agent Account</button>
+                        </div>
+                    </li>
+                </ul>
                 </fieldset>
             </form>
-        ) 
+        )
     }
     </RegisterComponent>
 )
