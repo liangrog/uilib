5ce2e239f75f77dd82c96eb1f8597ce8b235794c
Stephen Yuan <stephen@merlingroup.co>
2016-11-29 12:18:33 +1100

    search result and pagination components.

---
+++ react/components/pagination.js
@@ -0,0 +1,92 @@
+import React from 'react'
+
+
+class Pagination extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    changePerPage = (e) => {
+        this.props.onChangePerPage(parseInt(e.target.value))
+    }
+
+    changeCurPage = (e) => {
+        let toPage = parseInt(e.target.value)
+        if (toPage >= 1 && toPage <= this.props.maxPage) {
+            this.props.onChangeCurPage(toPage)
+        }
+    }
+
+    nextPage = (e) => {
+        e.preventDefault()
+        if (this.props.curPage < this.props.maxPage) {
+            this.props.onChangeCurPage(this.props.curPage + 1)
+        }
+    }
+
+    prevPage = (e) => {
+        e.preventDefault()
+        if (this.props.curPage > 1) {
+            this.props.onChangeCurPage(this.props.curPage - 1)
+        }
+    }
+
+    render = () => {
+        return (
+            <div className="product_listing_sort listing_bottom">
+                <div className="product_listing_sort_field">
+                    <div className="l_span_6">
+                        <div className="l_span_1">
+                            <span className="view">View</span>
+                        </div>
+                        <div className="l_span_3">
+                            <div className="select_wrap">
+                                <select value={this.props.perPage} onChange={this.changePerPage}>
+                                    <option value="5">5</option>
+                                    <option value="10">10</option>
+                                    <option value="20">20</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="l_span_2">
+                            <span className="per_page">per page</span>
+                        </div>
+                    </div>
+                    <div className="pgn l_span_6 l_last">
+                        <a href="#" className="i_prev" onClick={this.prevPage} disabled={this.props.curPage === 1}>
+                            <span>prev</span>
+                        </a>
+                        <span className="current">
+                            <input type="number"
+                                    className="input-text"
+                                    value={this.props.curPage}
+                                    onChange={this.changeCurPage}
+                                    maxLength="3"
+                            />
+                        </span>
+                        <a href="#" className="i_next" onClick={this.nextPage} disabled={this.props.curPage === this.props.maxPage}>
+                            <span>next</span>
+                        </a>
+                        <span className="total_pgn">
+                            of {this.props.maxPage} {(this.props.maxPage > 1) ? 'pages' : 'page'}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+Pagination.propTypes = {
+    curPage: React.PropTypes.number.isRequired,
+    perPage: React.PropTypes.number.isRequired,
+    maxPage: React.PropTypes.number.isRequired,
+    onChangeCurPage: React.PropTypes.func.isRequired,
+    onChangePerPage: React.PropTypes.func.isRequired
+}
+
+Pagination.defaultProps = {
+}
+
+export default Pagination
---
+++ react/components/search-result.js
@@ -0,0 +1,197 @@
+import React from 'react'
+import Immutable from 'immutable'
+
+import Pagination from './pagination'
+
+
+class SearchResult extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            perPage: props.defaultPrePage,
+            curPage: 1,
+            sorting: props.defaultSorting
+        }
+        this.checkboxes = []
+    }
+
+    displayingResult = () => {
+        let data = Immutable.fromJS(this.props.data)
+
+        // Sorting
+        if (this.state.sorting.attr && this.state.sorting.type) {
+            data = data.sortBy(d => d.get('name'))
+            if (this.state.sorting.type === 'desc') {
+                data = data.reverse()
+            }
+        }
+
+        // Paging
+        let from = (this.state.curPage - 1) * this.state.perPage
+        let to = from + this.state.perPage
+        data = data.slice(from, to)
+
+        return data.toJSON()
+    }
+
+    changeCurPage = (curPage) => {
+        this.setState({
+            curPage
+        })
+    }
+
+    changePerPage = (perPage) => {
+        this.setState({
+            perPage,
+            curPage: 1
+        })
+    }
+
+    changeSorting = (attr, type, e) => {
+        e.preventDefault()
+        this.setState({
+            sorting: {
+                attr,
+                type
+            }
+        })
+    }
+
+    toggleMainCheckbox = (e) => {
+        this.checkboxes.map(
+            cb => {
+                cb.checked = e.target.checked
+            }
+        )
+    }
+
+    headers = () => {
+        let cols = this.props.cols.map(
+            (col, i) => (
+                <th key={i}>
+                    {col.label}
+                    {
+                        (col.sorting) ?
+                            <span>
+                                <a href="#"
+                                        className="sort_up"
+                                        onClick={this.changeSorting.bind(null, col.attr, 'asc')}
+                                >
+                                    <span className={"i_up" + ((this.state.sorting.attr === col.attr && this.state.sorting.type === 'asc')? ' active' : '')}></span>
+                                </a>
+                                <a href="#"
+                                        className="sort_down"
+                                        onClick={this.changeSorting.bind(null, col.attr, 'desc')}
+                                >
+                                    <span className={"i_down" + ((this.state.sorting.attr === col.attr && this.state.sorting.type === 'desc')? ' active' : '')}></span>
+                                </a>
+                            </span>
+                        :
+                            null
+                    }
+                </th>
+            )
+        )
+
+        if (this.props.checkboxCol) {
+            cols.unshift(
+                <th key="checkboxCol">
+                    <input type="checkbox"
+                            onChange={this.toggleMainCheckbox}
+                    />
+                </th>
+            )
+        }
+
+        return cols
+    }
+
+    row = (obj) => {
+        let row = this.props.cols.map(
+            (col, i) => (
+                <td key={i}>
+                    {obj[col.attr]}
+                </td>
+            )
+        )
+
+        if (this.props.checkboxCol) {
+            row.unshift(
+                <td key="checkboxCol">
+                    <input type="checkbox"
+                            ref={checkbox => {this.checkboxes.push(checkbox)}}
+                            name={this.props.checkboxCol.name}
+                            value={obj[this.props.checkboxCol.value]}
+                    />
+                </td>
+            )
+        }
+
+        return row
+    }
+
+    rows = () => {
+        return this.displayingResult().map(
+            (obj, i) => (
+                <tr key={i}>
+                    {this.row(obj)}
+                </tr>
+            )
+        )
+    }
+
+    render = () => {
+        return (
+            <div>
+                <table className="stacktable">
+                    <thead>
+                        <tr>
+                            {this.headers()}
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {this.rows()}
+                    </tbody>
+                </table>
+
+                <Pagination
+                        curPage={this.state.curPage}
+                        perPage={this.state.perPage}
+                        maxPage={Math.ceil(this.props.data.length / this.state.perPage)}
+                        onChangeCurPage={this.changeCurPage}
+                        onChangePerPage={this.changePerPage}
+                        onChangeSorting={this.changeSorting}
+                />
+            </div>
+        )
+    }
+}
+
+SearchResult.propTypes = {
+    data: React.PropTypes.arrayOf(React.PropTypes.object),
+    cols: React.PropTypes.arrayOf(React.PropTypes.shape({
+        label: React.PropTypes.string.isRequired,
+        attr: React.PropTypes.string.isRequired,
+        sorting:  React.PropTypes.bool
+    })),
+    defaultPrePage: React.PropTypes.number,
+    defaultSorting: React.PropTypes.shape({
+        attr: React.PropTypes.string,
+        type:  React.PropTypes.oneOf(['asc', 'desc'])
+    }),
+    checkboxCol: React.PropTypes.shape({
+        name: React.PropTypes.string.isRequired,
+        valueAttr:  React.PropTypes.string,
+    }),
+}
+
+SearchResult.defaultProps = {
+    data: [],
+    cols: [],
+    defaultPrePage: 10,
+    defaultSorting: {}
+}
+
+export default SearchResult
