5d44de5c5c41938c44a366d5dfcc9c0b210b6263
JD <me@jdinh.life>
2017-04-07 16:05:35 +1000

    added pdf type & external handlers

--- react/components/file-upload.js
+++ react/components/file-upload.js
@@ -1,15 +1,25 @@
-import React, { Component, PropTypes } from 'react'
+import { Component, PropTypes } from 'react'
 
 import logger from '../../logger'
 
+const propTypes = {
+    acceptedTypes: PropTypes.object,
+    children: PropTypes.any,
+    dirName: PropTypes.string,
+    imgPreview: PropTypes.bool,
+    onFailure: PropTypes.func,
+    onSuccess: PropTypes.func,
+    uploadFile: PropTypes.func.isRequired
+}
 
 class FileUpload extends Component {
-    constructor(props) {
+    constructor (props) {
         super(props)
-        this.acceptedTypes = {
+        this.acceptedTypes = props.acceptedTypes || {
             'image/png': true,
             'image/jpeg': true,
-            'image/gif': true
+            'image/gif': true,
+            'application/pdf': true
         }
 
         this.state = {
@@ -20,9 +30,9 @@ class FileUpload extends Component {
     upload = e => {
         e.preventDefault()
 
-        let file = e.target.files[0]
-        let onFailure = (err) => { logger.log(err) }
-        let onSuccess = (data) => { logger.log(data)}
+        const file = e.target.files[0]
+        const onFailure = this.props.onFailure || ((err) => { logger.log(err) })
+        const onSuccess = this.props.onSuccess || ((data) => { logger.log(data) })
 
         if (this.props.imgPreview) {
             this.preview(file)
@@ -31,54 +41,23 @@ class FileUpload extends Component {
         if (this.acceptedTypes[file.type]) {
             this.props.uploadFile(file, this.props.dirName, onFailure, onSuccess)
         }
-
-        //todo handle upload to s3
     }
 
-    //preview
     preview = (file) => {
         let reader = new FileReader()
         reader.onloadend = () => {
             this.setState({
                 imageUrl: reader.result
-            });
+            })
         }
 
         reader.readAsDataURL(file)
     }
 
     render = () => this.props.children(this)
-/*    render = () => {
-        return (
-            <div>
-            <ul className="form-list">
-                <li>
-                    <label>Profile photo</label>
-                </li>
-            </ul>
-                <div className="img-container l_span_2">
-                    <img className="profile-preview" src={this.state.imageUrl} />
-                    <img className="profile-preview" src="../../assets/images/profile-preview.png" />
-                </div>
-                <div className="l_span_10 l_last">
-                    {<a className="" id="upload" onClick={() => this.refs.fileInput.click()}>{this.props.buttonLabel}</a>}
-                    <input className="btn btn_secondary btn_margin_small" type="file" name="files[]" ref="fileInput" onChange={this.upload}/>
-                    <p>You can also drag and drop a picture from your computer.</p>
-                    <div className="">
-                        <p><strong>Upload requirements</strong><br/>
-                        Photo dimensions 600 x 600 pixels.<br/>
-                        Maximum file size is 5MB and accepted file types are JPG, GIF and PNG.</p>
-                    </div>
-                </div>
-            </div>
-        )
-    }*/
 }
 
-FileUpload.propTypes = {
-    dirName: PropTypes.string,
-    imgPreview: PropTypes.bool
-}
+FileUpload.propTypes = propTypes
 
 FileUpload.defaultProps = {
     dirName: '',
